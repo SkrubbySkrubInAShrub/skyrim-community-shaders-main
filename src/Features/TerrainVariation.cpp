cc1615b6ccac165f760c686598391e71b1b13aad
David Kehoe <davidkehoe0411@outlook.com>
2026-06-08 20:28:07 +1000

    fix: cn translation & localisation merge

--- src/Features/TerrainVariation.cpp
+++ src/Features/TerrainVariation.cpp
@@ -1,8 +1,7 @@
 #include "TerrainVariation.h"
-#include "../FeatureBuffer.h"
-#include "../Globals.h"
-#include "../I18n/I18n.h"
-#include "../State.h"
+#include "I18n/I18n.h"
+#include "Menu.h"
+#include "Menu/Fonts.h"
 #include "../Util.h"
 
 #define I18N_KEY_PREFIX "feature.terrain_variation."
@@ -21,17 +20,18 @@ void TerrainVariation::DrawSettings()
 
 	ImGui::Spacing();
 
-	bool oldLODEnabled = settings.enableLODTerrainTilingFix;
-	ImGui::Checkbox(T(TKEY("apply_to_lod_terrain"), "Apply to LOD Terrain"), (bool*)&settings.enableLODTerrainTilingFix);
-	if (oldLODEnabled != (bool)settings.enableLODTerrainTilingFix) {
-		UpdateShaderSettings();
-		logger::info("TerrainVariation LOD setting changed to: {}", settings.enableLODTerrainTilingFix);
+	bool lodTilingFix = settings.enableLODTerrainTilingFix != 0;
+	if (ImGui::Checkbox(T(TKEY("apply_to_lod_terrain"), "Apply to LOD Terrain"), &lodTilingFix)) {
+		settings.enableLODTerrainTilingFix = lodTilingFix ? 1u : 0u;
+		logger::info("TerrainVariation LOD setting changed to: {}", settings.enableLODTerrainTilingFix != 0);
 	}
 	if (auto _tt = Util::HoverTooltipWrapper()) {
 		ImGui::Text("%s", T(TKEY("apply_to_lod_terrain_tooltip"),
 							  "Applies the tiling fix to LOD terrain objects.\nThis helps reduce the visible tiling effect on distant terrain."));
 	}
 }
+
+#undef I18N_KEY_PREFIX
 
 void TerrainVariation::PostPostLoad()
 {
